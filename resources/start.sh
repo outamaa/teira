a761c1a8ce2224cb5ef19ea848d0d5f6abc9eae6
Ville Outamaa <ville.outamaa@solita.fi>
2017-03-12 09:16:43 +0200

    add scripts from pi

---
+++ resources/start.sh
@@ -0,0 +1,3 @@
+sudo /home/pi/umtskeeper/umtskeeper --sakisoperators 'APN="CUSTOM_APN" CUSTOM_APN="internet" USBINTERFACE="0" USBDRIVER="option" MODEM="12d1:1446 12d1:1506" SIM_PIN="1234" APN_USER="1234" APN_PASS="1234"' --sakisswitches "--sudo --console"
+
+#CUSTOM_APN=internet|data.dna.fi|julkinen.dna.fi
